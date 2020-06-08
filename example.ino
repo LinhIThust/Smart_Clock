0959e01bbd827991058a90c3cb8b62175b9c1073
donglinhit <donglinhit.hust@gmail.com>
2020-06-09 00:45:32 +0700

    clear code

--- example.ino
+++ example.ino
@@ -1,11 +1,3 @@
-/**************************************************************************************
- 
- ESP8266 NodeMCU WiFi Internet clock and weather station and with SSD1306 OLED display
- This is a free software with NO WARRANTY.
- http://simple-circuit.com/
- 
-***************************************************************************************/
- 
 // ESP8266 WiFi main library
 #include <ESP8266WiFi.h>
  
@@ -27,10 +19,6 @@
 #define OLED_RESET   5     // define SSD1306 OLED reset at ESP8266 GPIO5 (NodeMCU D1)
 Adafruit_SSD1306 display(OLED_RESET);
  
-// set Wi-Fi SSID and password
-const char *ssid     = "TP-LINK_52ED32";
-const char *password = "44448888";
- 
 WiFiUDP ntpUDP;
 // 'time.nist.gov' is used (default server) with +1 hour offset (3600 seconds) 60 seconds (60000 milliseconds) update interval
 NTPClient timeClient(ntpUDP, "time.nist.gov", 3600, 60000);
@@ -38,11 +26,54 @@ NTPClient timeClient(ntpUDP, "time.nist.gov", 3600, 60000);
 // set location and API key
 String Location = "Hanoi";
 String API_Key  = "90366e0d41ba5f0fcac7621190876245";
+char Time[] = "  :  :  ";
+char Date[] = "  -  -20  ";
+byte last_second, last_minute, second_, minute_, hour_, wday, day_, month_, year_;
+
+void updateWearher();
+void configWifi();
+void updateTime();
+void configOled();
  
 void setup(void)
 {
   Serial.begin(9600);
   delay(1000);
+  configOled();
+  configWifi();
+  timeClient.begin();
+  
+  delay(1000);
+}
+ 
+
+ 
+void loop()
+{
+  if (WiFi.status() == WL_CONNECTED)  // check WiFi connection status
+  {
+    updateTime();
+    updateWeather();
+    delay(200);
+  }
+}
+
+
+void display_wday()
+{
+  switch(wday)
+  {
+    case 1:  display.print("SUNDAY    "); break;
+    case 2:  display.print("MONDAY    "); break;
+    case 3:  display.print("TUESDAY   "); break;
+    case 4:  display.print("WEDNESDAY "); break;
+    case 5:  display.print("THURSDAY  "); break;
+    case 6:  display.print("FRIDAY    "); break;
+    default: display.print("SATURDAY  ");
+  }
+ 
+}
+void configOled(){
  
   Wire.begin(4, 0);           // set I2C pins [SDA = GPIO4 (D2), SCL = GPIO0 (D3)], default clock is 100kHz
  
@@ -55,8 +86,10 @@ void setup(void)
   display.clearDisplay();   // clear the display buffer
   display.setTextColor(WHITE, BLACK);
   display.setTextSize(1);
-  display.display();
- 
+  display.display();  
+}
+
+void configWifi(){
   WiFiManager wifiManager;
   //reset settings - for testing
   wifiManager.autoConnect("My ESP");
@@ -65,24 +98,12 @@ void setup(void)
   Serial.println("connected...yeey :)");
   Serial.println("local ip");
   Serial.println(WiFi.localIP());
- 
   Serial.println("connected");
   display.print("connected");
   display.display();
- 
-  timeClient.begin();
- 
-  delay(1000);
 }
- 
-char Time[] = "  :  :  ";
-char Date[] = "  -  -20  ";
-byte last_second, last_minute, second_, minute_, hour_, wday, day_, month_, year_;
- 
-void loop()
-{
-  if (WiFi.status() == WL_CONNECTED)  // check WiFi connection status
-  {
+
+void updateTime(){
     timeClient.update();
     unsigned long unix_epoch = timeClient.getEpochTime();   // get UNIX Epoch time
     unix_epoch+=21600;
@@ -115,7 +136,7 @@ void loop()
       Serial.println("TIME:");
       display.setCursor(60, 0);
       display.print(Time);        // display time (format: hh:mm:ss)
-       Serial.println(Time);
+      Serial.println(Time);
       display.setCursor(0, 11);
       Serial.println("DATE:");
       display_wday();
@@ -124,23 +145,21 @@ void loop()
       display.display();
  
       last_second = second_;
-    }
- 
-    if (last_minute != minute_)       // update weather every 1 minute
+    } 
+  
+ }
+void updateWeather(){
+  if (last_minute != minute_)       // update weather every 1 minute
     {
       HTTPClient http;  // declare an object of class HTTPClient
- 
       // specify request destination
       http.begin("http://api.openweathermap.org/data/2.5/weather?q=" + Location + "&APPID=" + API_Key);  // !!
- 
       int httpCode = http.GET();  // send the request
  
       if (httpCode > 0)  // check the returning code
       {
         String payload = http.getString();   // get the request response payload
- 
         DynamicJsonBuffer jsonBuffer(512);
- 
         // Parse JSON object
         JsonObject& root = jsonBuffer.parseObject(payload);
         if (!root.success())
@@ -148,49 +167,23 @@ void loop()
           Serial.println(F("Parsing failed!"));
           return;
         }
- 
         float temp = (float)(root["main"]["temp"]) - 273.15;        // get temperature in °C
         int   humidity = root["main"]["humidity"];                  // get humidity in %
-        float pressure = (float)(root["main"]["pressure"]) / 1000;  // get pressure in bar
-        float wind_speed = root["wind"]["speed"];                   // get wind speed in m/s
-        int  wind_degree = root["wind"]["deg"];                     // get wind degree in °
- 
         display.setCursor(0, 24);
         Serial.print("Nhiệt độ:");
         display.printf("Temperature: %5.2f C\r\n", temp);
         Serial.println(temp);
+        Serial.print("Độ ẩm:");
         display.printf("Humidity   : %d %%\r\n", humidity);
-        display.printf("Pressure   : %.3fbar\r\n", pressure);
-        display.printf("Wind speed : %.1f m/s\r\n", wind_speed);
-        display.printf("Wind degree: %d", wind_degree);
+        Serial.println(humidity);
         display.drawRect(109, 24, 3, 3, WHITE);     // put degree symbol ( ° )
         display.drawRect(97, 56, 3, 3, WHITE);
         display.display();
- 
       }
  
       http.end();   // close connection
  
       last_minute = minute_;
     }
- 
-    delay(200);
- 
-  }
+  
 }
- 
-void display_wday()
-{
-  switch(wday)
-  {
-    case 1:  display.print("SUNDAY    "); break;
-    case 2:  display.print("MONDAY    "); break;
-    case 3:  display.print("TUESDAY   "); break;
-    case 4:  display.print("WEDNESDAY "); break;
-    case 5:  display.print("THURSDAY  "); break;
-    case 6:  display.print("FRIDAY    "); break;
-    default: display.print("SATURDAY  ");
-  }
- 
-}
-// End of code.
